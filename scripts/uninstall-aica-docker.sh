977c4186313d18ac99899d8f0c1b3684023bf11b
Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>
2021-10-05 14:59:17 +0200

    Robustify uninstall (#18)
    * The `test -f ${FILE}` line should return true for files and
    symlinks, but in case the aica-docker file in the bin is
    corrupted for whatever reason it might return false.
    It might as well always echo removal and use the safe
    rm -f removal, even if the file was not there.

--- scripts/uninstall-aica-docker.sh
+++ scripts/uninstall-aica-docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 SYMLINK=/usr/local/bin/aica-docker
-if test -f "$SYMLINK"; then
-  echo "Removing symbolic link ${SYMLINK}"
-  rm "${SYMLINK}"
-fi
+echo "Removing symbolic link ${SYMLINK}"
+rm -f "${SYMLINK}"
