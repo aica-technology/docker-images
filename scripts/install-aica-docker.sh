0eb79d07d54ad8d61fc8d366d1d98439021d8fbc
Dominic Reber <dominic.reber@epfl.ch>
2021-10-06 09:09:18 +0200

    Echo message when writing to rc script

--- scripts/install-aica-docker.sh
+++ scripts/install-aica-docker.sh
@@ -20,6 +20,7 @@ function get_shell_rc_path () {
 }
 
 function source_completion_script () {
+  echo "Writing the auto completion option to &1."
   grep -v /src/aica-docker-completion.sh "$1" > tmpfile && mv tmpfile "$1"
   if [[ "$OSTYPE" != "darwin"* ]]; then
     echo "autoload bashcompinit; bashcompinit; source ${SCRIPT_DIR}/src/aica-docker-completion.sh" >> "$1"
@@ -48,7 +49,7 @@ while true; do
   read -r -p "Do you with to install auto completion for aica-docker to '${SHELL_RC_PATH}'? [yes|no]
 [yes] >>>" yn
   case $yn in
-    yes) source_completion_script "${SHELL_RC_PATH}"; break;;
+    "" | yes) source_completion_script "${SHELL_RC_PATH}"; break;;
     no) exit;;
     *) echo "Please answer 'yes' or 'no'.";;
   esac
