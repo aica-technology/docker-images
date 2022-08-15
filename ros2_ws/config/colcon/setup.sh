9d837fa954955295b87842ec587bcc168dfc4183
Enrico Eberhard <enrico.eberhard@gmail.com>
2022-08-15 17:51:43 +0200

    Export ROS2_WORKSPACE variable in alias
    * To allow the colcon build / test subshells to access the
    ROS2_WORKSPACE variable in an SSH session, it must be exported

--- ros2_ws/config/colcon/setup.sh
+++ ros2_ws/config/colcon/setup.sh
@@ -4,6 +4,6 @@
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh; export _colcon_cd_root=${ROS2_WORKSPACE}" >> ~/.bashrc
 
 # add aliases
-echo "alias cb='bash ${COLCON_HOME}/build.sh'" >> "${HOME}"/.bash_aliases
-echo "alias ct='bash ${COLCON_HOME}/test.sh'" >> "${HOME}"/.bash_aliases
+echo "alias cb='export ROS2_WORKSPACE; /bin/bash ${COLCON_HOME}/build.sh'" >> "${HOME}"/.bash_aliases
+echo "alias ct='export ROS2_WORKSPACE; /bin/bash ${COLCON_HOME}/test.sh'" >> "${HOME}"/.bash_aliases
 echo "alias ccd=colcon_cd" >> "${HOME}"/.bash_aliases
