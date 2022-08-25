df69fdc31d65f60d71de910c493fb58cd3557e07
Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>
2022-08-25 16:00:48 +0200

    Correctly escape COLCON_WORKSPACE variable in bashrc (#51)
    * Correctly escape the workspace variable in bashrc;
    Use single quotes to escape the variable written to bashrc
    so that the value is only expanded at the login step (at which
    point the value may have changed)
    
    * Rename workspace env variable to COLCON_WORKSPACE
    (previously ROS2_WORKSPACE)

--- ros2_ws/config/colcon/setup.sh
+++ ros2_ws/config/colcon/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # enable the colcon_cd utility
-echo "source /usr/share/colcon_cd/function/colcon_cd.sh; export _colcon_cd_root=${ROS2_WORKSPACE}" >> ~/.bashrc
+echo 'source /usr/share/colcon_cd/function/colcon_cd.sh; export _colcon_cd_root="${COLCON_WORKSPACE}"' >> ~/.bashrc
 
 # add aliases
 echo "alias cb='export ROS2_WORKSPACE; /bin/bash ${COLCON_HOME}/build.sh'" >> "${HOME}"/.bash_aliases
