eb655b3f2ae53609f978384f173608c4c09c805f
Dominic Reber <dominic@aica.tech>
2023-06-27 20:03:21 +0200

    Fix order in build-local

--- build-local.sh
+++ build-local.sh
@@ -40,13 +40,13 @@ done
 
 cd "${SCRIPT_DIR}"/ros2_ws && bash ./build.sh --base-tag "${BASE_TAG}" "${BUILD_FLAGS[@]}" || exit 1
 
+cd "${SCRIPT_DIR}"/ros2_control && bash ./build.sh --local-base \
+  --base-tag "${BASE_TAG}" --output-tag "${BASE_TAG}" "${BUILD_FLAGS[@]}" || exit 1
+
 cd "${SCRIPT_DIR}"/ros2_control_libraries && bash ./build.sh --local-base --cl-branch "${CL_BRANCH}" \
   --base-tag "${BASE_TAG}" --output-tag "${OUTPUT_TAG}" "${BUILD_FLAGS[@]}" || exit 1
 
-cd "${SCRIPT_DIR}"/ros2_modulo && bash ./build.sh --local-base --modulo-branch "${MODULO_BRANCH}" \
-  --base-tag "${OUTPUT_TAG}" --output-tag "${OUTPUT_TAG}" "${BUILD_FLAGS[@]}" || exit 1
-
-cd "${SCRIPT_DIR}"/ros2_modulo_control && bash ./build.sh --local-base \
+cd "${SCRIPT_DIR}"/ros2_modulo_control && bash ./build.sh --local-base --modulo-branch "${MODULO_BRANCH}" \
   --base-tag "${OUTPUT_TAG}" --output-tag "${OUTPUT_TAG}" "${BUILD_FLAGS[@]}" || exit 1
 
 cd "${CURRENT_DIR}"
