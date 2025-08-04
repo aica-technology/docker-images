2fae66598b194d23fb1892b51c0b65f82d2f4e14
Vaios Papaspyros <8146703+bpapaspyros@users.noreply.github.com>
2025-08-04 10:36:19 +0200

    fix: change target lib path to /usr

--- toolkits/Dockerfile.ml
+++ toolkits/Dockerfile.ml
@@ -235,7 +235,7 @@ ARG PYTHON_VERSION
 ARG CPP_DEPS
 ARG PY_DEPS
 
-COPY --from=apt-fetch /deps/lib /usr/local/lib
+COPY --from=apt-fetch /deps/lib /usr/lib
 COPY --from=apt-fetch /deps/bin /usr/bin
 COPY --from=cpu-builder ${CPP_DEPS}/lib/ /usr/lib/
 COPY --from=cpu-builder ${CPP_DEPS}/include/ /usr/include/
