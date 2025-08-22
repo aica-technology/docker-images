705a80036381b72e4cf509013e92d6bdfd4e0c69
Vaios Papaspyros <8146703+bpapaspyros@users.noreply.github.com>
2025-08-22 22:21:54 +0200

    merge scripts

--- yolo_model_converter/build-run.sh
+++ yolo_model_converter/build-run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-IMAGE_NAME=ghcr.io/aica-technology/yolo_model_converter
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+IMAGE_NAME=ghcr.io/aica-technology/yolo_model_converter
+VERSION=$(cat VERSION)
+MODEL="yolo12n.pt"
+
+
 BUILD_FLAGS=()
 while [ "$#" -gt 0 ]; do
   case "$1" in
@@ -14,6 +18,10 @@ while [ "$#" -gt 0 ]; do
     BUILD_FLAGS+=(--progress=plain)
     shift 1
     ;;
+  --model)
+    MODEL=$2
+    shift 2
+    ;;
   *)
     echo "Unknown option: $1" >&2
     exit 1
@@ -23,3 +31,11 @@ done
 
 VERSION=$(cat "${SCRIPT_DIR}"/VERSION)
 docker buildx build -t "${IMAGE_NAME}":v"${VERSION}" "${BUILD_FLAGS[@]}" .
+
+docker run --rm -it \
+    -e UID=$(id -u) \
+    -e GID=$(id -g) \
+    -e model="${MODEL}" \
+    -v .:/exports \
+    --name yolo_model_converter \
+    ghcr.io/aica-technology/yolo_model_converter:v${VERSION}
